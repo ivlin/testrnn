c40c09f4ad1883b6a0ce046658ef317d43140bdd
ivlin <ivlin112@gmail.com>
2018-08-01 10:26:29 -0400

    langrnn

--- rnntest.py
+++ rnntest.py
@@ -1,7 +1,103 @@
-#import numpy as np
-#import tensorflow as tf
+import numpy as np
+import tensorflow as tf
+
+#constants
+SEQUENCE_LENGTH = 40
+step = 3
+
+#load data
 
 with open("lolita_part1.txt","r",encoding="utf-8") as file:
     text=file.read()
+chars = sorted(list(set(text)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+print(f'unique chars: {len(chars)}')
+
+sentences = []
+next_chars = []
+for i in range(0, len(text) - SEQUENCE_LENGTH, step):
+    sentences.append(text[i: i + SEQUENCE_LENGTH])
+    next_chars.append(text[i + SEQUENCE_LENGTH])
+print(f'num training examples: {len(sentences)}')
+
+#model
     
-print(str(len(text)))
+def RNN(x):
+    with tf.variable_scope("RNN"):
+        rnn_cell = tf.nn.rnn_cell.BasicLSTMCell(128)
+        rnn_cell_out = rnn_cell.apply(x)
+        out = tf.layers.dense(rnn_cell_out, len(chars))
+    return out
+
+x=tf.placeholder([None,SEQUENCE_LENGTH,len(chars)], dtype=t.bool)
+labels=tf.placeholder([None,len(chars)], dtype=t.bool)
+y=RNN(x)
+loss=tf.nn.softmax_cross_entropy_loss_with_logits(labels=labels, logits=y)
+optimizer=tf.train.RMSPropOptimizer(0.01)
+train_steps=optimizer.minimize(loss)
+
+#train
+X = np.zeros((len(sentences), SEQUENCE_LENGTH, len(chars)), dtype=np.bool)
+Y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+sess=tf.Session()
+sess.run(tf.global_variables_initializer())
+
+predict=sess.run([train_steps], feed_dict={x:X, labels:Y})
+
+#test
+
+def prepare_input(text):
+    x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
+    for t, char in enumerate(text):
+        x[0, t, char_indices[char]] = 1.
+    return x
+    
+def sample(preds, top_n=3):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+    
+def predict_completion(text):
+    original_text = text
+    generated = text
+    completion = ''
+    while True:
+        x = prepare_input(text)
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, top_n=1)[0]
+        next_char = indices_char[next_index]
+        text = text[1:] + next_char
+        completion += next_char
+        
+        if len(original_text + completion) + 2 > len(original_text) and next_char == ' ':
+            return completion
+            
+def predict_completions(text, n=3):
+    x = prepare_input(text)
+    preds = model.predict(x, verbose=0)[0]
+    next_indices = sample(preds, n)
+    return [indices_char[idx] + predict_completion(text[1:] + indices_char[idx]) for idx in next_indices]
+    
+    
+quotes = [
+    "It is not a lack of love, but a lack of friendship that makes unhappy marriages.",
+    "That which does not kill us makes us stronger.",
+    "I'm not upset that you lied to me, I'm upset that from now on I can't believe you.",
+    "And those who were seen dancing were thought to be insane by those who could not hear the music.",
+    "It is hard enough to remember my opinions, without also remembering my reasons for them!"
+]
+for q in quotes:
+    seq = q[:40].lower()
+    print(seq)
+    print(predict_completions(seq, 5))
+    print()
